c36c45ac2befeba47b572e8b0758f1ed21241170
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-11-24 15:09:29 +0100

    first commit

---
+++ src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.ecommerce.microcommerce.controller;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    // Couche données
+    @Autowired
+    private ProductDao productDao;
+
+    // Produits
+    @GetMapping(value = "Produits")
+    public List<Product> productList() {
+        return productDao.findAll();
+    }
+
+    // Produits/{id}
+    @GetMapping(value = "Produits/{id}")
+    public Product getProduit(@PathVariable int id) {
+        return productDao.findById(id);
+    }
+
+    // Post Product
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Void> addProduct(@RequestBody Product product) {
+        Product productSend = productDao.save(product);
+
+        if (product == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productSend.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+}
